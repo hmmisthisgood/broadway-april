f27699176d39ba123305d488a7a5e9c32e9764e7
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-05-04 08:09:16 +0545

    added oop

--- basics/oop/user.dart
+++ basics/oop/user.dart
@@ -7,7 +7,7 @@ class User {
       required this.email,
       required this.role,
       required String ccNumber})
-      : _creditCardNumber = ccNumber;
+      : this._creditCardNumber = ccNumber;
 
   int id;
   String username;
@@ -21,12 +21,94 @@ class User {
 
   set ccN(String value) => _creditCardNumber = value;
 
+  static String country = "Nepal";
+  static peformTask() {}
+
   login() {
     print("Yay I'm logged in: ${this.username}");
   }
 }
 
 // object/instance
+
+/// Admin = sub class
+/// User= super class or base class
+class Admin extends User {
+  Admin({
+    required int age,
+    required int id,
+    required String email,
+    required String username,
+    required String ccNumber,
+    required this.phoneNo,
+  }) : super(
+            age: age,
+            id: id,
+            email: email,
+            username: username,
+            ccNumber: ccNumber,
+            role: "admin");
+
+  String phoneNo;
+
+  deleteUser() {}
+  updateUser() {}
+  blockUser() {}
+
+  @override
+  login() {
+    print("I am logging in from admin");
+    // super.login();
+  }
+}
+
+class Client extends User {
+  Client({
+    required int age,
+    required int id,
+    required String email,
+    required String username,
+    required String ccNumber,
+  }) : super(
+            age: age,
+            id: id,
+            email: email,
+            username: username,
+            ccNumber: ccNumber,
+            role: "client");
+
+  uploadPhoto() {
+    print(" i am uploading photo");
+  }
+
+  sendMessage() {
+    print(" Message sent");
+  }
+
+  @override
+  login() {
+    print("\n i am loggin in from Cleint");
+
+    // return super.login();
+  }
+}
+
+abstract class Geometry {
+  area(int length, int breadth);
+  perimeter(int legth, int breadth);
+}
+
+class Square extends Geometry {
+  @override
+  area(int length, int breadth) {
+    return length * length;
+  }
+
+  @override
+  perimeter(int legth, int breadth) {
+    return 4 * legth;
+  }
+}
 
 main() {
   User jamesBond = new User(
@@ -43,7 +125,7 @@ main() {
 
   print("James Bonds details: Name : ${jamesBond.username}");
 
-  jamesBond.login();
+  // jamesBond.login();
 
   jamesBond.ccN = "11111 1111";
 
@@ -59,5 +141,29 @@ main() {
       role: "role",
       ccNumber: "1111 5555");
 
-  spiderMan.login();
+  // spiderMan.login();
+
+  Admin admin = Admin(
+      age: 50,
+      id: 1233123,
+      email: "admin@admin.com",
+      username: "iamadmin",
+      phoneNo: "9841000000",
+      ccNumber: "1244242");
+
+  admin.login();
+  admin.deleteUser();
+
+  Client client = Client(
+      age: 50,
+      id: 1233123,
+      email: "cleint@cleint.com",
+      username: " i am clinet",
+      ccNumber: "1244242");
+  client.login();
+  // client.uploadPhoto();
+  // client.sendMessage();
+
+  User.country;
+  User.peformTask();
 }
