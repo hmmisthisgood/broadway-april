8a69e925de033c8dbaf78459ba2f4e815e83b3b7
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-05-29 07:52:23 +0545

    json serrializtion of nested objects done

--- http_basics/lib/main.dart
+++ http_basics/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'homepage.dart';
+import 'screen/user_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      home: UsersListScreen(),
     );
   }
 }
---
+++ http_basics/lib/model/company.dart
@@ -0,0 +1,18 @@
+//  { "name": "Romaguera-Crona",
+//   "catchPhrase": "Multi-layered client-server neural-net",
+//   "bs": "harness real-time e-markets"
+// }
+
+class Company {
+  final String name, catchPhrase, bs;
+
+  Company({required this.name, required this.catchPhrase, required this.bs});
+
+  factory Company.convert(Map json) {
+    return Company(
+      name: json['name'],
+      catchPhrase: json['catchPhrase'],
+      bs: json['bs'],
+    );
+  }
+}
