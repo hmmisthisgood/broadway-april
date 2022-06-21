93c213c0ceedfe905a29bf1b95c4313ab425f292
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-06-21 07:09:42 +0545

    implemented lottie animation

---
+++ animation/lib/screen/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        Lottie.asset("assets/animation/apple_animation.json"),
+        Image.asset("assets/animation/applecut.gif")
+      ]),
+    );
+  }
+}
